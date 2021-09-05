071a16693a0f11ee2aeaf93c5695954ffcfcf0ee
dibaliqaja <dibaliqaja@gmail.com>
2021-09-05 21:26:30 +0700

    refresh database and env.testing

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -2,22 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
-    // /**
-    //  * product page contains empty
-    //  *
-    //  * @return void
-    //  */
-    // public function test_product_page_contains_empty_products_table()
-    // {
-    //     $response = $this->get('/product');
+    use RefreshDatabase;
 
-    //     $response->assertStatus(200);
-    //     $response->assertSee('No products found.');
-    // }
+    /**
+     * product page contains empty
+     *
+     * @return void
+     */
+    public function test_product_page_contains_empty_products_table()
+    {
+        $response = $this->get('/product');
+
+        $response->assertStatus(200);
+        $response->assertSee('No products found.');
+    }
 
     /**
      * product page contains non empty
@@ -26,9 +30,15 @@ class ProductsTest extends TestCase
      */
     public function test_product_page_contains_non_empty_products_table()
     {
+        $product = Product::create([
+            'name' => 'Bottle',
+            'price' => 5000
+        ]);
+
         $response = $this->get('/product');
 
         $response->assertStatus(200);
         $response->assertDontSee('No products found.');
+        $response->assertSee($product->name);
     }
 }
